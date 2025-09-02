333c717357fbc213fdada5d5e3d46ef8753440b5
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-02 09:30:13 +0100

    feat: add header and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import React from "react"
 
-/**
- * Project planning:
- * 
- * Questions to ask yourself before writing any code:
- * 
- * - What are the main containers of elements I need
- *   in this app?
- * 
- * 
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * 
- * 
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * 
- * 
- */
 
-export default function Hangman() {
+
+export default function AssembleyEndgame() {
     return (
         <main>
-            Game goes here
+            <header>
+              <h1>Assembly: Endgame</h1>
+            
+              <p>Guess the word in under 8 attemps to keep the programming world safe from Assembley!</p>
+            </header>
         </main>
     )
 }
