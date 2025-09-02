24842ccd54c5ef986c75bf0b00f61a972db1927e
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-02 10:19:24 +0100

    feat: add lanuguages array

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import React from "react"
-
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Build a status section below the header.
- * For now, you can just hard-code in the styles for
- * a winning game, and we'll make it more dynamic
- * later.
- */
+import { languages } from "./lanuguages"
 
 export default function AssembleyEndgame() {
-    return (
-        <main>
-            <header>
-              <h1>Assembly: Endgame</h1>
-            
-              <p>Guess the word in under 8 attemps to keep the programming world safe from Assembley!</p>
-            </header>
-            <section className="game-status">
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
-            </section>
-        </main>
+  const lanuguageChip=languages.map(lang=>{
+    const styles={
+      backgroundColor:lang.backgroundColor,
+      color:lang.color
+    }
+    return(
+      <span
+      key={lang.name} 
+      className="chip" 
+      style={styles}
+      > {lang.name} </span>
     )
+  })
+  return (
+      <main>
+          <header>
+            <h1>Assembly: Endgame</h1>
+          
+            <p>Guess the word in under 8 attemps to keep the programming world safe from Assembley!</p>
+          </header>
+          <section className="game-status">
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+          </section>
+          <section className="lanuguage-chips">
+            {lanuguageChip}
+          </section>
+      </main>
+  )
 }
