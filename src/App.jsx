6224cf185b01d1802a84518e0d22561453ff15bd
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-02 09:38:09 +0100

    improve the styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 
-
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Build a status section below the header.
+ * For now, you can just hard-code in the styles for
+ * a winning game, and we'll make it more dynamic
+ * later.
+ */
 
 export default function AssembleyEndgame() {
     return (
@@ -10,6 +17,10 @@ export default function AssembleyEndgame() {
             
               <p>Guess the word in under 8 attemps to keep the programming world safe from Assembley!</p>
             </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
